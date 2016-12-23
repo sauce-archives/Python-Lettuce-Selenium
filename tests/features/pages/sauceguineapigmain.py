6c62921d042a10e528d0ab569b5877c37c16cba3
ndmanvar <ndmanvar@yahoo.com>
2016-12-22 19:52:33 -0800

    Use https://saucelabs-sample-test-frameworks.github.io/training-test-page

--- tests/features/pages/sauceguineapigmain.py
+++ tests/features/pages/sauceguineapigmain.py
@@ -5,13 +5,13 @@ class SauceGuineaPig(Page):
   SauceGuineaPig class that represents the guienea pig webpage
   """
   def __init__(self, selenium_driver):
-    Page.__init__(self, selenium_driver, 'https://saucelabs.com/test/guinea-pig')
+    Page.__init__(self, selenium_driver, 'https://saucelabs-sample-test-frameworks.github.io/training-test-page')
 
   def find_unchecked_checkbox(self):
     return self.driver.find_element_by_id('unchecked_checkbox')
 
   def find_email_input(self):
-    return self.driver.find_element_by_id('fbemail') 
+    return self.driver.find_element_by_id('fbemail')
 
   def find_link(self):
     return self.driver.find_element_by_tag_name('a')
